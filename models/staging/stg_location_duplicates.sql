4e688d38505a6df6d405cde11df34ce46e56cc6a
Gabriel Zenkner <gabriel.zenkner@gmail.com>
2023-11-21 18:15:50 +0000

    changed staging to JSON_EXTRACT raw data from string

--- models/staging/stg_location_duplicates.sql
+++ models/staging/stg_location_duplicates.sql
@@ -1,20 +1,21 @@
 with source as (
 
-    select * from {{ source('weather_api', 'weather_api_table') }}
+    select * from {{ source('weather_api', 'weather_api_table_raw') }}
 
 ),
 
 renamed as (
 
     select
-        name,
-        base,
-        timezone,
-        coord.lat AS lat,
-        coord.lon AS lon
-
+        REGEXP_REPLACE(JSON_EXTRACT(string, '$.name'), r'"', '') AS city_name,
+        REGEXP_REPLACE(JSON_EXTRACT(string, '$.base'), r'"', '') AS base,
+        CAST(JSON_EXTRACT(string, '$.timezone') AS FLOAT64) AS timezone,
+        CAST(JSON_EXTRACT(string, '$.coord.lat') AS FLOAT64) AS lat,
+        CAST(JSON_EXTRACT(string, '$.coord.lon') AS FLOAT64) AS lon,
+        REGEXP_REPLACE(JSON_EXTRACT(string, '$.sys.country'), r'"', '') AS country
+    
     from source
-    GROUP BY name, base, timezone, lat, lon
+    GROUP BY city_name, base, timezone, lat, lon, country
 
 
 )
--- models/staging/stg_weather.sql
+++ models/staging/stg_weather.sql
@@ -9,17 +9,12 @@ renamed as (
     select
         CAST(JSON_EXTRACT(string, '$.dt') AS INT64) AS dt,
         REGEXP_REPLACE(JSON_EXTRACT(string, '$.name'), r'"', '') AS city_name,
-        CAST(JSON_EXTRACT(string, '$.coord.lon') AS FLOAT64) AS lon,
-        CAST(JSON_EXTRACT(string, '$.coord.lat') AS FLOAT64) AS lat,
-        CAST(JSON_EXTRACT(string, '$.main.temp') AS FLOAT64) AS temp_k,
-        CAST(JSON_EXTRACT(string, '$.main.pressure') AS FLOAT64) AS pressure,
-        CAST(JSON_EXTRACT(string, '$.main.humidity') AS FLOAT64) AS humidity,
-        CAST(JSON_EXTRACT(string, '$.wind.speed') AS FLOAT64) AS speed,
-        CAST(JSON_EXTRACT(string, '$.wind.deg') AS FLOAT64) AS deg,
         CAST(JSON_EXTRACT(string, '$.clouds.all') AS FLOAT64) AS cloud_cover,
-        REGEXP_REPLACE(JSON_EXTRACT(string, '$.base'), r'"', '') AS base,
-        REGEXP_REPLACE(JSON_EXTRACT(string, '$.sys.country'), r'"', '') AS country,
-        CAST(JSON_EXTRACT(string, '$.timezone') AS FLOAT64) AS timezone,
+        CAST(JSON_EXTRACT(string, '$.wind.deg') AS FLOAT64) AS wind_deg,
+        CAST(JSON_EXTRACT(string, '$.wind.speed') AS FLOAT64) AS wind_speed,
+        CAST(JSON_EXTRACT(string, '$.main.pressure') AS FLOAT64) AS pressure,
+        CAST(JSON_EXTRACT(string, '$.main.temp') AS FLOAT64) AS temp_k,
+        CAST(JSON_EXTRACT(string, '$.main.humidity') AS FLOAT64) AS humidity,
         CAST(JSON_EXTRACT(string, '$.visibility') AS FLOAT64) AS visibility
 
     from source
